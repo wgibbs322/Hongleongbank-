821f342ac86c4ee795726eec85d385433498c591
wgibbs3237 <wgibbs3237@gmail.com>
2025-02-08 12:13:32 -0800

    bank

--- fetchdisplaytransaction.js
+++ fetchdisplaytransaction.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', async () => {
     try {
-        const response = await fetch('https://hongleongbankbackend.onrender.com/api/transactions');
+        const response = await fetch('https://hongleongbackenddd.onrender.com/api/transactions');
         const transactions = await response.json();
 
         if (response.ok) {
